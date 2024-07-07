728042bfb218f3e32fb555ca6891903b9b808307
Jeanee Allen <allenjeanee@yahoo.com>
2024-07-07 16:04:56 -0500

    created a function to verify if a year is a leap year

--- whiteboard.js
+++ whiteboard.js
@@ -34,4 +34,20 @@ console.log(fizzBuzz(7));
 console.log(fizzBuzz(10));
 console.log(fizzBuzz(15));
 
+//Question 3: Write a program that takes in a year (as an integer) and returns true if the year is a leap year.
+//Years that are divisible by 4 are leap years, return true
+//Years that are divisible by 100 are not leap years, return false
+//Years that are divisible by 400 are leap years, return true
+
+function leapYear(year) {
+    if (year % 400 === 0) { return true }
+    else if (year % 100 === 0) { return false }
+    else if (year % 4 === 0) { return true }
+    else return false;
+}
+console.log(leapYear(2024));
+console.log(leapYear(2004));
+console.log(leapYear(2017));
+console.log(leapYear(2200));
+
 

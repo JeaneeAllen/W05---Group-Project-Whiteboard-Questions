aa482f1c6544aaf8f0466f578ead647220dc98b9
Jeanee Allen <allenjeanee@yahoo.com>
2024-07-07 19:21:14 -0500

    created a function that use a for loop to determine if a sandwich is a BLT.

--- whiteboard.js
+++ whiteboard.js
@@ -50,4 +50,36 @@ console.log(leapYear(2004));
 console.log(leapYear(2017));
 console.log(leapYear(2200));
 
+// Question 4: Write a function that accepts an array of ingredients, and determines whether they make a BLT sandwich.
+//An array is a sandwich if the first and last items are both "bread",
+//and the middle contains "bacon", "lettuce" and "tomatoes".
 
+
+function sandwichBLT(ingredients) {
+    let bacon = false;
+    let lettuce = false;
+    let tomatoes = false;
+    if (ingredients.length < 2 || ingredients[0] !== 'bread' || ingredients[ingredients.length - 1] !== 'bread') {
+        breadOnBothSides = false
+        return "Its not a BLT";
+    }
+
+    for (let i = 1; i < ingredients.length - 1; i++) {
+        if (ingredients[i] === "bacon") {
+            bacon = true;
+        } else if (ingredients[i] === "lettuce") {
+            lettuce = true;
+        } else if (ingredients[i] === "tomatoes") {
+            tomatoes = true;
+        }
+    }
+    if (bacon && lettuce && tomatoes) {
+        return "Its a BLT!";
+    } else {
+        return "Its not a BLT";
+    }
+}
+
+console.log(sandwichBLT(["bread", "bacon", "lettuce", "blue cheese", "tomatoes", "bread"]));
+console.log(sandwichBLT(["bacon", "lettuce", "blue cheese", "tomatoes", "bread"]));
+console.log(sandwichBLT(["bread", "bacon", "blue cheese", "tomatoes", "bread"]));
